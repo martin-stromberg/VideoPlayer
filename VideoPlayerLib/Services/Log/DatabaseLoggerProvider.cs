496815a4bff905fa05d9aff7f9cd346dfa5f39fd
Muesli84 <m.stromberg@web.de>
2023-10-07 19:38:27 +0200

    Fehlende Datei fürs Protokoll hinzugefügt

---
+++ VideoPlayerLib/Services/Log/DatabaseLoggerProvider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoPlayerLib.Services.Database;
+
+namespace VideoPlayerLib.Services.Log
+{
+    public class DatabaseLoggerProvider : ILoggerProvider
+    {
+        public DatabaseLoggerProvider(IServiceProvider serviceProvider)
+            :base()
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        private ConcurrentDictionary<string, ILogger> _loggers = new ConcurrentDictionary<string, ILogger>();
+        private readonly IServiceProvider serviceProvider;
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            var logger = _loggers.GetOrAdd(categoryName, new Logger(serviceProvider.GetService<ILogDatabase>())) as Logger;
+            logger.CategoryName = categoryName;
+            return logger;
+        }
+
+        public void Dispose()
+        {
+            _loggers.Clear();
+        }
+    }
+}

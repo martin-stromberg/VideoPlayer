336ada04701baf20d0d53fa7fcb47b0de8883cc5
Muesli84 <m.stromberg@web.de>
2023-10-07 19:09:02 +0200

    Fehlende Klasse für das Protokoll hinzugefügt

---
+++ MyVideoPlayer/ViewModels/Logs/LogEntryBoxViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MyVideoPlayer.Helper.LibraryScan;
+using MyVideoPlayer.ViewModels.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoPlayerLib.Services.Database.Models;
+
+namespace MyVideoPlayer.ViewModels.Logs
+{
+    internal class LogEntryBoxViewModel: BaseMediaElementBoxViewModel
+    {
+        private LogEntry entry;
+
+        public LogEntryBoxViewModel(LogEntry entry) : base(null)
+        {
+            this.entry = entry;
+        }
+
+        public override string ToString()
+        {
+            return entry.ToString();
+        }
+    }
+}

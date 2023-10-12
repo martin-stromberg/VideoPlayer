a7b38b5cd81958061c1219d42b671ed7c415b0e7
Muesli84 <m.stromberg@web.de>
2023-10-12 19:50:41 +0200

    Exception-Handling beim FTP-Scan

---
+++ VideoPlayerLib/Services/Common/ExceptionEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace VideoPlayerLib.Services.Common
+{
+    public class ExceptionEventArgs: EventArgs
+    {
+
+        public ExceptionEventArgs(Exception error)
+            : base()
+        {
+            Error = error;
+        }
+
+        public Exception Error { get; }
+
+    }
+
+}

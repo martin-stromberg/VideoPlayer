ca0c0b44baecdca9a5a1335c012a521c7c758875
Muesli84 <m.stromberg@web.de>
2023-10-23 18:56:10 +0200

    Beim Datenexport werden MediaInformation nun als Json-Text ausgegeben

--- VideoPlayerMaui/Models/MetaInformation/MediaInformation.cs
+++ VideoPlayerMaui/Models/MetaInformation/MediaInformation.cs
@@ -1,7 +1,16 @@
-﻿namespace VideoPlayer.Models.MetaInformation
+﻿using Newtonsoft.Json;
+
+namespace VideoPlayer.Models.MetaInformation
 {
     public class MediaInformation
     {
+
         public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
     }
 }

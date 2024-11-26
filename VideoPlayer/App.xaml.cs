8ba29b32edf5847dbe43b969c5ef71b764d3f7a2
Muesli84 <m.stromberg@web.de>
2024-11-26 19:12:11 +0100

    ErrorLog

--- VideoPlayer/App.xaml.cs
+++ VideoPlayer/App.xaml.cs
@@ -1,16 +1,25 @@
 ﻿using VideoPlayer.Properties;
 using VideoPlayer.Service;
+using VideoPlayer.Service.ErrorHandling;
 
 namespace VideoPlayer
 {
     public partial class App: Application
     {
+        private readonly IErrorLogManager _errorLogManager;
 
         public App(IApplicationManager appInitialize)
         {
             InitializeComponent();
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
             MainPage = new AppShell(appInitialize);
+            _errorLogManager = appInitialize.ResolveService<IErrorLogManager>();
+        }
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.IsTerminating)
+                _errorLogManager.WriteError(e.ExceptionObject as Exception);
         }
 
         protected override void OnSleep()
